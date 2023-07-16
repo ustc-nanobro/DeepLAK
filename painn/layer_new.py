e5e09c4c338d975b623253bd97b3ee722d9875bb
2531018979@qq.com <2531018979@qq.com>
2023-07-16 14:57:54 +0800

    更新完成的代码

---
+++ painn/layer_new.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import einops
+class GaussianEmbedLayer(nn.Module):
+    def __init__(self, num_gauss, x_min, x_max, sigma, num_feats, mlp_hid_dim):
+        super().__init__()
+        self.num_gauss = num_gauss
+        self.x_min = x_min
+        self.x_max = x_max
+        self.sigma = sigma
+        self.num_feats = num_feats
+        self.mlp_hid_dim=mlp_hid_dim
+
+        if mlp_hid_dim:
+            mlp_layers = []
+            for i in range(len(mlp_hid_dim) - 1):
+                mlp_layers.append(nn.Linear(mlp_hid_dim[i], mlp_hid_dim[i+1]))
+                mlp_layers.append(nn.ReLU())
+            self.mlp = nn.Sequential(*mlp_layers)
+        else:
+            self.mlp = None
+
+    def gaussian(self, x, mean):
+        return torch.exp(-(x - mean) ** 2 / (2 * self.sigma ** 2))
+
+    def embed(self, x):
+        batch_size, num_points, num_feats = x.size()
+
+        #将输入重塑为 (batch_size * num_points, num_feats) 的形状
+        #x = x.view(-1, num_feats)
+        x = einops.rearrange(x, 'b n f -> (b n) f')
+        #print(x)
+        # 对输入向量的每个元素应用高斯函数
+        means = torch.linspace(self.x_min, self.x_max, self.num_gauss).unsqueeze(0)  # (1, num_gauss)
+        #print(means)
+        #x_gauss = self.gaussian(x, means)
+        x_gauss = self.gaussian(x.unsqueeze(2), means)  # (batch_size * num_points, num_feats, num_gauss)
+        #print(x_gauss)
+        # 将形状重新调整为 (batch_size, num_points, num_feats, num_gauss)
+        x_gauss = x_gauss.view(batch_size, num_points, num_feats, self.num_gauss)
+        #x_gauss = einops.rearrange(x_gauss, '(b n) f g -> b n f g')
+        return x_gauss
+        
+    def forward(self, x):
+        x_gauss = self.embed(x)
+        #print(x_gauss)
+        #print(x_gauss.size())
+        x_gauss = einops.rearrange(x_gauss, 'b n f g -> b (n f g)')  # Reshape to (batch_size, num_points * num_feats * num_gauss)
+        x_gauss = self.mlp(x_gauss)
+        
+        
+        return x_gauss
+
+
+  
---
+++ painn/test_layer_new.py
@@ -0,0 +1,38 @@
+import torch
+import pytest
+from layer_new import GaussianEmbedLayer
+ 
+num_feats = 4
+num_gauss = 4
+x_min = 0
+x_max = 1.0
+sigma = 0.5
+mlp_hid_dim = [64,128]
+
+def test_gaussian_embed_layer():
+   
+    batch_size=3
+    num_points=4
+
+   
+    model=GaussianEmbedLayer(num_gauss, x_min, x_max, sigma, num_feats, mlp_hid_dim)
+    x = torch.randn(batch_size, num_points, num_feats)
+    output = model(x)
+    #print(output.size())
+    assert output.shape == (batch_size, mlp_hid_dim[-1])
+
+def test_embed_layer():
+    feat=[[[0.0,0.3333,0.6666,1]]]
+    feat=torch.tensor(feat)
+    
+    
+    model=GaussianEmbedLayer(num_gauss, x_min, x_max, sigma, num_feats, mlp_hid_dim)
+    x_gauss=model.embed(feat)
+    #print(x_gauss)
+
+    is_symmetric = torch.allclose(x_gauss, x_gauss.transpose(-1, -2),rtol=1e-3, atol=1e-3)
+    print("whether it is a symmetric matrix",is_symmetric)
+
+
+test_embed_layer()
+test_gaussian_embed_layer()
